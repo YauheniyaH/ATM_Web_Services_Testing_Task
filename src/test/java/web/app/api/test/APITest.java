69945b731b49ad05ec00757823ecf6efa934fc7a
Yauheniya Hladkaya <yauheniya_hladkaya@epam.com>
2024-11-04 22:43:59 +0100

    parallel execution implemented with testNG xml file by tests User class deleted as redundant Content-type and status code updated with as static imports Added endpoint to properties file

--- src/test/java/web/app/api/test/APITest.java
+++ src/test/java/web/app/api/test/APITest.java
@@ -1,37 +1,51 @@
 package web.app.api.test;
 
 import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasSize;
 
 public class APITest {
-    private static final String ENDPOINT = "https://jsonplaceholder.typicode.com/users";
 
     private ValidatableResponse response;
 
+    private final Properties properties = new Properties();
+
     @BeforeMethod(alwaysRun = true)
     public void getMethodExecute() {
-        response = given().when().get(ENDPOINT).then();
+        try (InputStream input = APITest.class.getClassLoader().getResourceAsStream("endpoint.properties")) {
+            properties.load(input);
+            response = given().when().get(properties.getProperty("endpoint")).then();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Test
     public void getUsersStatusCodeTest() {
-        response.log().body();
+        response.log().status();
         response.assertThat().
-                statusCode(200);
+                statusCode(HttpStatus.SC_OK);
     }
 
     @Test
     public void getUsersHeaderTest() {
+        response.log().headers();
         response.assertThat().
-                header("Content-Type", "application/json; charset=utf-8");
+                header(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8");
     }
 
     @Test
     public void getUsersBodyTest() {
+        response.log().body();
         response.assertThat().
                 body("users", hasSize(10));
     }
