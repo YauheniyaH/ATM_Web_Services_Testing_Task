85b297e8892d49d59dd4e30414b521150ac8406e
Yauheniya Hladkaya <yauheniya_hladkaya@epam.com>
2024-11-07 10:54:03 +0100

    updated static imports

--- src/test/java/web/app/api/test/APITest.java
+++ src/test/java/web/app/api/test/APITest.java
@@ -1,8 +1,6 @@
 package web.app.api.test;
 
 import io.restassured.response.ValidatableResponse;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -11,10 +9,12 @@ import java.io.InputStream;
 import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpHeaders.CONTENT_TYPE;
+import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.Matchers.hasSize;
 
 public class APITest {
-
+    private static final int TOTAL_USERS = 10;
     private ValidatableResponse response;
 
     private final Properties properties = new Properties();
@@ -23,7 +23,9 @@ public class APITest {
     public void getMethodExecute() {
         try (InputStream input = APITest.class.getClassLoader().getResourceAsStream("endpoint.properties")) {
             properties.load(input);
+            //create request and use .prettyPrint() to log  request
             response = given().when().get(properties.getProperty("endpoint")).then();
+            response.log();
         } catch (IOException exception) {
             exception.printStackTrace();
         }
@@ -31,22 +33,19 @@ public class APITest {
 
     @Test
     public void getUsersStatusCodeTest() {
-        response.log().status();
         response.assertThat().
-                statusCode(HttpStatus.SC_OK);
+                statusCode(SC_OK);
     }
 
     @Test
     public void getUsersHeaderTest() {
-        response.log().headers();
         response.assertThat().
-                header(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8");
+                header(CONTENT_TYPE, "application/json; charset=utf-8");
     }
 
     @Test
     public void getUsersBodyTest() {
-        response.log().body();
         response.assertThat().
-                body("users", hasSize(10));
+                body("users", hasSize(TOTAL_USERS));
     }
 }
